a070401be841a032ffd89da9a242da1d6c966fe8
Pablo Apablaza <papablaza@student.unimelb.edu.au>
2025-12-08 18:19:37 +1100

    Cleanup and addition of PISP example

---
+++ tutorials/PISP - Dataset example.jl
@@ -0,0 +1,13 @@
+using PISP
+
+PISP.build_ISP24_datasets( 
+                        downloadpath = normpath(@__DIR__, "pisp-downloads"),    # Path where all files from AEMO's website will be downloaded and extracted
+                        download_from_AEMO = true,           # Whether to download files from AEMO's website
+                        poe          = 10,                    # Probability of exceedance (POE) for demand: 10% or 50%
+                        reftrace     = 2018,                  # Reference weather year trace: select among 2011 - 2023 or 4006 (trace for the ODP)
+                        years        = [2030,2031,2032,2040,2050],    # Calendar years for which to build the time-varying schedules: select among 2025 - 2050
+                        output_name  = "out",                 # Output folder name   
+                        output_root  = normpath(@__DIR__, "pisp-datasets"),
+                        write_csv    = true,                  # Whether to write CSV files
+                        write_arrow  = false,                 # Whether to write Arrow files
+                    )    
